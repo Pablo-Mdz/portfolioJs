fa107c16d687ff35acf7dbe4a3f337d66e6e8221
Pablo Cigoy <pablocigoy@gmail.com>
2023-01-02 20:20:16 +0100

    projects and about, email not working

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -1,49 +1,65 @@
-import React from 'react'
+// import React from 'react'
 import { contact, section5Title, social } from '../../profile'
+// import emailjs from "emailjs-com"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-    
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_tvfybt7', form.current, '1d-j2JW1QjE-26RcM')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
+
     return (
         <div className="parallax">
-                <div data-aos="zoom-in-up" data-aos-once="true" className="git-form">
-                    <>
-                <div className="git-head-div text-center mx-auto">
+            <div data-aos="zoom-in-up" data-aos-once="true" className="git-form">
+                <>
+                    <div className="git-head-div text-center mx-auto">
                         <h1 id="Contact" className="git-head">{section5Title}</h1>
-                </div>
+                    </div>
                 </>
                 <div className="container">
-            <div className="git-cont row">
-                <div className="col-12 col-sm-6 half">
-                    <form action={contact.contactUrl ? contact.contactUrl : "https://formspree.io"} method={contact.contactUrl ? "POST" : "GET"}>
-                        <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
-                        <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
-                        <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
-                        <textarea id="msg" name="message" placeholder="Message" required></textarea>
-                        <button style={{cursor: 'pointer'}} type="submit">Send Message</button>
-                    </form>
-                </div>
-                <div className="col-12 col-sm-6 half">
-                    <p className="lead">
-                        {contact.pitch}        
-                    </p>
-                <div className="d-flex justify-content-center align-items-center flex-column">
-                <div className="inline-block">
-                    {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
-                    {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
-                    {social.twitter && <a title="Visit Twitter profile" rel="noopener noreferrer" target="_blank" href={social.twitter}><i className="fab fa-twitter"></i></a>}
-                    {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
-                    {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br/>
-                </div>
-                    {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
-                </div>
+                    <div className="git-cont row">
+                        <div className="col-12 col-sm-6 half">
+                            <form ref={form} onSubmit={sendEmail} /*action={contact.contactUrl ? contact.contactUrl : "https://formspree.io"} method={contact.contactUrl ? "POST" : "GET"}*/>
+                                <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
+                                <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
+                                <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
+                                <textarea id="msg" name="message" placeholder="Message" required></textarea>
+                                <button style={{ cursor: 'pointer' }} type="submit">Send Message</button>
+                            </form>
+                        </div>
+                        <div className="col-12 col-sm-6 half">
+                            <p className="lead">
+                                {contact.pitch}
+                            </p>
+                            <div className="d-flex justify-content-center align-items-center flex-column">
+                                <div className="inline-block">
+                                    {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
+                                    {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
+                                    {social.twitter && <a title="Visit Twitter profile" rel="noopener noreferrer" target="_blank" href={social.twitter}><i className="fab fa-twitter"></i></a>}
+                                    {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
+                                    {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br />
+                                </div>
+                                {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
             <p id="not-dark" className="Copy">2023 © Copyright <strong>{contact.copyright}</strong>. All Rights Reserved</p>
         </div>
     )
-    
+
 }
 
 export default Contact
